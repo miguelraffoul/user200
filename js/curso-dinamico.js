a4e299dd9ab65e64e929f5fa3141bf254dd4d122
Miguel Angel Aguirre Raffoul <magraffoul@gmail.com>
2013-11-01 18:15:35 -0600

    Horario dinamico registro curso

---
+++ js/curso-dinamico.js
@@ -0,0 +1,46 @@
+function agregarDiaCurso( opcion ) {
+	var opciones_select = document.getElementById( 'dias_curso' ).getElementsByTagName( 'option' );
+	for( var i = 0; i < opciones_select.length; ++i ){
+		if( opciones_select[i].value == opcion.value ) {
+			opciones_select[i].setAttribute( 'disabled', 'disabled' );
+			break;
+		}
+	}
+
+	var plantilla = document.getElementById( 'dia_curso_template' );
+	var nuevo_dia = plantilla.cloneNode();
+	nuevo_dia.removeAttribute( 'style' );
+	nuevo_dia.setAttribute( 'id', opcion.value );
+	var label = nuevo_dia.getElementsByTagName( 'p' );
+	label[0].appendChild( document.createTextNode( diaValor( opcion.value ) ) );
+
+	document.getElementById( 'alta_curso_der' ).appendChild( nuevo_dia );
+} 
+
+function diaValor( valor ){
+	switch( valor ) {
+		case '1':
+			return 'Lunes';
+		case '2':
+			return 'Martes';
+		case '3':
+			return 'Miércoles';
+		case '4':
+			return 'Jueves';
+		case '5':
+			return 'Viernes'
+		case '6':
+			return 'Sábado';
+	}
+}
+
+function eliminarDiaCurso( dia ){
+	var opciones_select = document.getElementById( 'dias_curso' ).getElementsByTagName( 'option' );
+	for( var i = 0; i < opciones_select.length; ++i ){
+		if( opciones_select[i].value == dia.parentNode.id ) {
+			opciones_select[i].removeAttribute( 'disabled' );
+			break;
+		}
+	}
+	document.getElementById( 'alta_curso_der' ).removeChild( dia.parentNode );
+}
