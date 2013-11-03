7cffd18615dd1feabf163f45b6c3cce38a3dc27f
Miguel Angel Aguirre Raffoul <magraffoul@gmail.com>
2013-11-03 12:42:16 -0600

    Cambio pass

--- Modelo/CambioContraMdl.php
+++ Modelo/CambioContraMdl.php
@@ -9,34 +9,32 @@ class CambioContraMdl {
 	}
 
 	function esAlumno( $pass, $nuevo_pass ) {
-		$consulta = "SELECT * FROM alumno WHERE password = \"$pass\"";
-		if( $this -> bd -> consultaEspecifica( $consulta ) ) {
-			$consulta = "UPDATE alumno SET password = \"$nuevo_pass\" WHERE password = \"$pass\"";
-			$this -> bd -> insertar( $consulta );
-			return true;
-		}
+		$consulta = "UPDATE alumno SET password = \"$nuevo_pass\" WHERE password = \"$pass\"";
+		$result = $this -> bd -> insertar( $consulta );
+		var_dump( $result );
+		if( $result )
+			if( $this -> bd -> affectedRows() > 0 )
+				return true;
 		return false;
 	}
 
 	function esProfesor( $pass, $nuevo_pass ) {
-		$consulta = "SELECT * FROM profesor WHERE password = \"$pass\"";
-		if( $this -> bd -> consultaEspecifica( $consulta ) ) {
-			echo "es profesor";
-			$consulta = "UPDATE profesor SET password = \"$nuevo_pass\" WHERE password = \"$pass\"";
-			$this -> bd -> insertar( $consulta );
-			return true;
-		}
+		$consulta = "UPDATE profesor SET password = \"$nuevo_pass\" WHERE password = \"$pass\"";
+		$result = $this -> bd -> insertar( $consulta );
+		var_dump( $result );
+		if( $result )
+			if( $this -> bd -> affectedRows() )
+				return true;
 		return false;	
 	}
 
 	function esAdministrador( $pass, $nuevo_pass ) {
-		$consulta = "SELECT * FROM administrador WHERE password = \"$pass\"";
-		if( $this -> bd -> consultaEspecifica( $consulta ) ) {
-			echo "es admin";
-			$consulta = "UPDATE administrador SET password = \"$nuevo_pass\" WHERE password = \"$pass\"";
-			$this -> bd -> insertar( $consulta );
-			return true;
-		}
+		$consulta = "UPDATE administrador SET password = \"$nuevo_pass\" WHERE password = \"$pass\"";
+		$result = $this -> bd -> insertar( $consulta );
+		var_dump( $result );
+		if( $result )
+			if( $this -> bd -> affectedRows() )
+				return true;
 		return false;
 	}
 	
