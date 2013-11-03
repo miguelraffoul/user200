c83466080523d556c466bbcc641f1801966d51e3
RosaIss <rosa_14_7@hotmail.com>
2013-11-02 23:47:01 -0600

    ciclo nuevo controlador

---
+++ Controlador/CicloNuevoCtl.php
@@ -0,0 +1,62 @@
+<?php
+
+class CicloNuevoCtl{
+
+	private $modelo;
+
+	public function ejecutar(){
+
+		require_once("Modelo/CicloNuevoMdl.php");
+		$this -> modelo = new CicloNuevoMdl();
+
+		switch ($_GET['act']){
+
+			case "agregar_ciclo":
+
+				if(empty($_POST)){
+					require_once("Vista/CicloNuevo.html");
+				}
+				else{
+
+					
+					$ciclo_select = $_POST["ciclo_select"];
+					$inicio_ciclo = $_POST["inicio_ciclo"];
+					$fin_ciclo = $_POST["fin_ciclo"];
+					$fd_inhabil = $_POST["fecha_dia_inhabil"];
+					$descripcion = $_POST["descripcion"];
+
+					array_pop( $fd_inhabil );
+					array_pop( $descripcion );
+
+					$fi_ciclo = $this -> darFormatoFecha( $inicio_ciclo ); 
+					$ff_ciclo = $this -> darFormatoFecha( $fin_ciclo ); 
+
+					$longitud = count( $fd_inhabil );
+					for( $i = 0 ; $i < $longitud ; $i = $i + 1 ){
+						$fd_inhabil[$i] = $this -> darFormatoFecha( $fd_inhabil[$i] );
+						$descripcion[$i] = trim( $descripcion[$i] );
+					}
+
+					$resultado = $this -> modelo -> agregarCiclo($ciclo_select, $fi_ciclo, $ff_ciclo, $fd_inhabil, $descripcion);
+
+					if($resultado!==FALSE)
+						require_once("Vista/CicloNuevo.html");
+					else
+						echo "NO SE PUDO";
+						//require_once("Vista/Error.html");
+				}
+				break;
+
+			default:
+				require_once("Vista/404.html");
+		}
+	}
+
+
+	function darFormatoFecha( $fecha ){
+
+		$fecha_datos = explode( "/", $fecha );
+		return $fecha_datos[2] . "-" . $fecha_datos[1] . "-" . $fecha_datos[0];
+	}
+
+}
---
+++ Modelo/CicloNuevoMdl.php
@@ -0,0 +1,47 @@
+<?php
+
+class CicloNuevoMdl{
+	private $bd;
+
+	function __construct(){
+		require_once( "BaseDeDatos.php" );
+		$this -> bd = BaseDeDatos::obtenerInstancia();
+	}
+
+	function agregarCiclo( $ciclo_select, $fi_ciclo, $ff_ciclo, $fd_inhabil, $descripcion ){
+
+		$consulta = "INSERT INTO cicloescolar ( idCicloEscolar, inicio, fin, Administrador_codigo ) 
+					 VALUES ( \"$ciclo_select\", \"$fi_ciclo\", \"$ff_ciclo\", '123admin')";
+
+		$consulta_exitosa = $this -> bd -> insertar( $consulta );
+
+		var_dump( $consulta_exitosa );
+
+		if( $consulta_exitosa ){
+			$consulta_exitosa = $this -> agregarDiasInhabiles( $ciclo_select, $fd_inhabil, $descripcion );
+			return $consulta_exitosa;	
+		}
+		else
+			return FALSE;
+	}
+
+
+	function agregarDiasInhabiles( $ciclo_select, $fd_inhabil, $descripcion ){
+
+		$longitud = count( $fd_inhabil );
+
+		for( $i = 0 ; $i < $longitud ; $i = $i + 1 ){
+			$temp_dia = $fd_inhabil[$i];
+			$temp_desc = $descripcion[$i];
+			$consulta = "INSERT INTO diainhabil (fecha, motivo, CicloEscolar_idCicloEscolar )
+					 	 VALUES ( \"$temp_dia\", \"$temp_desc\", \"$ciclo_select\" )";
+
+			$consulta_exitosa = $this -> bd -> insertar( $consulta );
+
+			if( $consulta_exitosa === FALSE )
+				return FALSE;
+		}
+
+		return TRUE;
+	}
+}
